ba5e81bdd40236df54940893e428c655f94ca7dd
odavidola <davidolagunju@hotmail.co.uk>
2023-12-05 11:03:54 +0000

    overflow auto

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,7 +31,7 @@ export default function RootLayout(
       <link rel="icon" href="favicon.ico"/>
     </Head>
     <body>
-    <div className="flex min-h-screen bg-noise text-white bg-cover bg-center bg-no-repeat z-[-1] overflow-scroll">
+    <div className="flex min-h-screen bg-noise text-white bg-cover bg-center bg-no-repeat z-[-1] overflow-auto">
       <Navbar/>
       <main className="w-3/4 h-screen p-5">
         {children}
