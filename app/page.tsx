355f7cccc7ff2324ac7236d1922e59f9e57d0a17
odavidola <davidolagunju@hotmail.co.uk>
2024-07-24 06:28:47 +0100

    change positioning of highlights and link

--- app/page.tsx
+++ app/page.tsx
@@ -1,34 +1,49 @@
-'use client';
-import React, {FC, Suspense} from 'react';
-import {ContactLink} from "@/app/components/contactlink";
-import {GitHubIcon, LinkedInIcon, TwitterIcon} from "@/app/components/icons";
-import {MediaHover} from "@/app/components/mediahover";
+"use client";
+import React, { FC, Suspense } from "react";
+import { ContactLink } from "@/app/components/contactlink";
+import { GitHubIcon, LinkedInIcon, TwitterIcon } from "@/app/components/icons";
+import { MediaHover } from "@/app/components/mediahover";
 
 const HomePage: FC = () => {
   return (
     <section id="intro">
       <div className="flex flex-col justify-between items-end h-full">
-        <div className="contact-details text-sm">
-          <ContactLink name={"Twitter"} link={'https://twitter.com/ODavidOlagunju'} icon={<TwitterIcon/>}/>
-          <ContactLink name={"LinkedIn"} link={'https://www.linkedin.com/in/david-olagunju-143aa8a7/'}
-                       icon={<LinkedInIcon/>}/>
-          <ContactLink name={"Github"} link={'https://github.com/odavidola'} icon={<GitHubIcon/>}/>
-        </div>
         <div className="flex ">
-          <Suspense fallback={<></>}>
-            <MediaHover/>
-          </Suspense>
           <div className="vertical-text text-white flex items-center text-xs">
             HIGHLIGHTS
           </div>
+          <Suspense fallback={<></>}>
+            <MediaHover />
+          </Suspense>
         </div>
+        <div className="contact-details text-sm">
+          <ContactLink
+            name={"Twitter"}
+            link={"https://twitter.com/ODavidOlagunju"}
+            icon={<TwitterIcon />}
+          />
+          <ContactLink
+            name={"LinkedIn"}
+            link={"https://www.linkedin.com/in/david-olagunju-143aa8a7/"}
+            icon={<LinkedInIcon />}
+          />
+          <ContactLink
+            name={"Github"}
+            link={"https://github.com/odavidola"}
+            icon={<GitHubIcon />}
+          />
+        </div>
+
         <div className="text-sm mt-10 w-125">
           <p>Born in 1996</p>
           <p>in Ibadan, Nigeria.</p>
-          <br/>
-          <p>I believe the world is only as large as our perception of it. Cultivating our
-            awareness expands the scope, not just of the material at our disposal to create
-            from, but of the life we get to live.</p>
+          <br />
+          <p>
+            I believe the world is only as large as our perception of it.
+            Cultivating our awareness expands the scope, not just of the
+            material at our disposal to create from, but of the life we get to
+            live.
+          </p>
         </div>
       </div>
     </section>
