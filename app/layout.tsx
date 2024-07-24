76242c81551d7f78b24e9ecbd43a800381de38e5
odavidola <davidolagunju@hotmail.co.uk>
2024-07-24 06:32:21 +0100

    deploying analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,44 +1,49 @@
-import './globals.css'
-import clsx from 'clsx';
-import type {Metadata} from 'next'
+import "./globals.css";
+import clsx from "clsx";
+import type { Metadata } from "next";
 
 import Navbar from "@/app/components/navbar";
 import React from "react";
 import Head from "next/head";
+import { Analytics } from "@vercel/analytics/react";
 
 export const metadata: Metadata = {
-  metadataBase: new URL('https://odola.xyz'),
-  title: 'David Olagunju - Creator & Developer',
-  description: 'Explore the portfolio of David Olagunju, a budding creator and developer.',
-  keywords: 'David Olagunju, creator, developer, portfolio',
+  metadataBase: new URL("https://odola.xyz"),
+  title: "David Olagunju - Creator & Developer",
+  description:
+    "Explore the portfolio of David Olagunju, a budding creator and developer.",
+  keywords: "David Olagunju, creator, developer, portfolio",
   robots: {
     index: true,
-    follow: true
-  }
-}
+    follow: true,
+  },
+};
 
-export default function RootLayout(
-  {
-    children,
-  }: {
-    children: React.ReactNode
-  }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
-    <html lang="en" className={clsx(
-      'text-black bg-white dark:text-white dark:bg-[#111010] border border-white m-5',
-    )}>
-    <Head>
-      <title>David Olagunju - Creator & Developer</title>
-      <link rel="icon" href="favicon.ico"/>
-    </Head>
-    <body>
-    <div className="flex min-h-screen bg-noise text-white bg-cover bg-center bg-no-repeat z-[-1] overflow-auto">
-      <Navbar/>
-      <main className="w-3/4 h-screen p-5">
-        {children}
-      </main>
-    </div>
-    </body>
+    <html
+      lang="en"
+      className={clsx(
+        "text-black bg-white dark:text-white dark:bg-[#111010] border border-white m-5",
+      )}
+    >
+      <Head>
+        <title>David Olagunju - Creator & Developer</title>
+        <link rel="icon" href="favicon.ico" />
+      </Head>
+      <body>
+        <div className="flex min-h-screen bg-noise text-white bg-cover bg-center bg-no-repeat z-[-1] overflow-auto">
+          <Navbar />
+          <main className="w-3/4 h-screen p-5">
+            {children}
+            <Analytics />
+          </main>
+        </div>
+      </body>
     </html>
-  )
+  );
 }
